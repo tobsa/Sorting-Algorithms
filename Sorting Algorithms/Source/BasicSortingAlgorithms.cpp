6645646d450c313dacb9ced9a91e5df684a44c4c
Tobias Savinainen <tcs1000@hotmail.com>
2012-09-19 15:47:48 +0200

    Added insertion sort

--- Sorting Algorithms/Source/BasicSortingAlgorithms.cpp
+++ Sorting Algorithms/Source/BasicSortingAlgorithms.cpp
@@ -27,7 +27,7 @@ void selectionSort(std::vector<int>& v)
 	for(std::size_t i = 0; i < v.size() - 1; ++i)
 	{
 		std::size_t min = i; // Current index with smallest value
-		for(std::size_t j = i + 1; j < v.size() - 1; ++j)
+		for(std::size_t j = i + 1; j < v.size(); ++j)
 		{
 			// If a smaller element was found than the element in the current index, then mark it as the smallest
 			if(v[j] < v[min])
@@ -35,5 +35,24 @@ void selectionSort(std::vector<int>& v)
 		}
 
 		std::swap(v[i], v[min]);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void insertionSort(std::vector<int>& v)
+{
+	for(std::size_t i = 1; i < v.size(); ++i)
+	{
+		int value = v[i];
+		int j = i - 1;
+
+		// Move down smaller values
+		while(j >= 0 && v[j] > value)
+		{
+			v[j + 1] = v[j];
+			--j;
+		}
+
+		v[j + 1] = value;
 	}
 }
